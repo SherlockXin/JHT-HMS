071b4d993bb35c4b0ccdd3f5065862dc88d2522d
zhang.xin <296442062@qq.com>
2018-11-05 09:18:07 +0800

    完成菜单管理查询、新增、修改、删除功能

--- src/api/menu.js
+++ src/api/menu.js
@@ -7,3 +7,33 @@ export function getMenu(param) {
     data: param
   })
 }
+
+export function selectMenu() {
+  return request({
+    url: '/sys/menu/select',
+    method: 'get'
+  })
+}
+
+export function getMenuInfo(id) {
+  return request({
+    url: '/sys/menu/info/' + id,
+    method: 'get'
+  })
+}
+
+export function saveOrUpdateMenu(menuInfo) {
+  return request({
+    url: menuInfo.menuId ? '/sys/menu/update' : '/sys/menu/save',
+    method: 'post',
+    data: menuInfo
+  })
+}
+
+export function cancelMenu(menuId) {
+  return request({
+    url: '/sys/menu/delete',
+    method: 'get',
+    params: { menuId }
+  })
+}
--- src/views/menu/index.vue
+++ src/views/menu/index.vue
@@ -1,79 +1,114 @@
 <template>
-  <div class="app-container">
-    <el-input v-model="filterText" placeholder="Filter keyword" style="margin-bottom:30px;" />
-
-    <el-tree
-      ref="tree2"
-      :data="menuList"
-      :props="defaultProps"
-      :filter-node-method="filterNode"
-      class="filter-tree"
-      default-expand-all
-    />
-
+  <div class="menu-container">
+    <el-button type="primary" size="medium" icon="el-icon-plus" @click="add">新增</el-button>
+    <tree-table :data="menuList" :current-change="selectChange" :columns="columns">
+      <el-table-column label="类型">
+        <template slot-scope="scope">
+          <el-tag v-if="scope.row.type == 0" type="info">{{menuType[scope.row.type]}}</el-tag>
+          <el-tag v-if="scope.row.type == 1">{{menuType[scope.row.type]}}</el-tag>
+          <el-tag v-if="scope.row.type == 2" type="success">{{menuType[scope.row.type]}}</el-tag>
+        </template>
+      </el-table-column>
+      <el-table-column label="排序">
+        <template slot-scope="scope">
+          {{scope.row.orderNum}}
+        </template>
+      </el-table-column>
+      <el-table-column label="图标">
+        <template slot-scope="scope">
+          <svg-icon :icon-class="scope.row.icon || ''"/>
+        </template>
+      </el-table-column>
+      <el-table-column label="路径">
+        <template slot-scope="scope">
+          {{scope.row.path}}
+        </template>
+      </el-table-column>
+      <el-table-column label="操作">
+        <template slot-scope="scope">
+          <div class="action-col">
+            <el-button type="primary" size="mini" icon="el-icon-edit" plain round @click="edit(scope.row.menuId)"></el-button>
+            <el-button size="mini" icon="el-icon-delete" round @click="cancel(scope.row.menuId)"></el-button>
+          </div>
+        </template>
+      </el-table-column>
+    </tree-table>
+    <el-dialog
+      :title="title"
+      :visible.sync="formVisible"
+      width="500px"
+      :close-on-click-modal="false"
+      :before-close="handleClose">
+      <el-form v-loading="formLoading" :model="menuInfo" :rules="formRules" size="small" :label-width="formLabelWidth" ref="menuForm">
+        <el-form-item label="菜单名称" prop="name">
+          <el-input v-model="menuInfo.name" placeholder="菜单名称或按钮名称"></el-input>
+        </el-form-item>
+        <el-form-item label="菜单类型" prop="type">
+          <el-radio v-for="(type,key) in menuType" v-model="menuInfo.type" :label="key">{{type}}</el-radio>
+          <el-radio-group v-model="menuInfo.type">
+            <el-radio v-for="(type,key) in menuType" :label="key">{{type}}</el-radio>
+          </el-radio-group>
+          <el-radio-group v-model="menuInfo.type">
+            <el-radio :label="0">目录</el-radio>
+            <el-radio :label="1">菜单</el-radio>
+            <el-radio :label="2">按钮</el-radio>
+          </el-radio-group>
+        </el-form-item>
+        <el-form-item label="上级菜单" prop="parentName">
+          <el-input v-model="menuInfo.parentName" placeholder="选择上级菜单" readonly @focus="selectPMenu"></el-input>
+        </el-form-item>
+        <el-form-item v-if="menuInfo.type != 2" label="菜单路径" prop="path">
+          <el-input v-model="menuInfo.path" placeholder="菜单路径"></el-input>
+        </el-form-item>
+        <el-form-item label="授权标识">
+          <el-input v-model="menuInfo.perms" type="textarea" autosize placeholder="多个用逗号分隔，如：user:list,user:create"></el-input>
+        </el-form-item>
+        <el-form-item label="排序号">
+          <el-input v-model="menuInfo.orderNum" type="number" placeholder="排序号"></el-input>
+        </el-form-item>
+        <el-form-item label="图标">
+          <el-input v-model="menuInfo.icon" placeholder="图标"></el-input>
+        </el-form-item>
+      </el-form>
+      <span slot="footer" class="dialog-footer">
+        <el-button @click="handleClose">取 消</el-button>
+        <el-button type="primary" @click="save" :loading="formLoading">确 定</el-button>
+      </span>
+    </el-dialog>
+    <el-dialog title="选择上级菜单" :visible.sync="pMenuVisible" width="300px">
+      <el-tree v-loading="pMenuLoading" :data="selectableMenu" :props="props" default-expand-all @node-click="pMenuClick"></el-tree>
+    </el-dialog>
   </div>
 </template>
 
 <script>
-import { getMenu } from '@/api/menu'
+import { getMenu, selectMenu, getMenuInfo, saveOrUpdateMenu, cancelMenu } from "@/api/menu";
+import treeTable from "@/components/TreeTable";
+
 export default {
+  components: { treeTable },
   data() {
     return {
-      filterText: "",
-      data2: [
-        {
-          id: 1,
-          label: "Level one 1",
-          children: [
-            {
-              id: 4,
-              label: "Level two 1-1",
-              children: [
-                {
-                  id: 9,
-                  label: "Level three 1-1-1"
-                },
-                {
-                  id: 10,
-                  label: "Level three 1-1-2"
-                }
-              ]
-            }
-          ]
-        },
-        {
-          id: 2,
-          label: "Level one 2",
-          children: [
-            {
-              id: 5,
-              label: "Level two 2-1"
-            },
-            {
-              id: 6,
-              label: "Level two 2-2"
-            }
-          ]
-        },
-        {
-          id: 3,
-          label: "Level one 3",
-          children: [
-            {
-              id: 7,
-              label: "Level two 3-1"
-            },
-            {
-              id: 8,
-              label: "Level two 3-2"
-            }
-          ]
-        }
-      ],
-      menuList:[],
-      defaultProps: {
-        children: "list",
-        label: "name"
+      menuInfo:{type:null,parentId:null,parentName:null},
+      title:'新增菜单',
+      formVisible: false,
+      pMenuVisible: false,
+      pMenuLoading: false,
+      formLoading: false,
+      formLabelWidth: '100px',
+      menuType: null,
+      columns: [{text: "名称",value: "name",width: 200}],
+      menuList: [],
+      selectableMenu: [],
+      props: {
+        label: "name",
+        value: "menuId"
+      },
+      formRules: {
+        name: [{required: true, message: '请输入菜单名称', trigger: 'blur'}],
+        type: [{required: true, message: '请输选择菜单类型', trigger: 'blur'}],
+        parentName: [{required: true, message: '请选择上级菜单', trigger: 'blur'}],
+        path: [{required: true, message: '请输入菜单路径', trigger: 'blur'}]
       }
     };
   },
@@ -82,22 +117,108 @@ export default {
       this.$refs.tree2.filter(val);
     }
   },
-
+  filters: {},
   methods: {
-    filterNode(value, data) {
-      if (!value) return true;
-      return data.label.indexOf(value) !== -1;
-    },
     getMenuList() {
-      getMenu().then( response => {
-        this.menuList = response
-        console.log(this.menuList)
+      getMenu().then(response => {
+        this.menuList = response.menuList;
+      });
+    },
+    getSelectableMenu() {
+      selectMenu().then(response => {
+        this.selectableMenu = response.menuList
       })
+    },
+    selectChange(currentRow, oldCurrentRow) {
+      this.currentRow = currentRow;
+      console.log("currentRow:", currentRow);
+    },
+    selectPMenu() {
+      this.pMenuVisible = true
+      this.pMenuLoading = true
+      selectMenu().then(response => {
+        this.selectableMenu = response.menuList
+        this.pMenuLoading = false
+      })
+    },
+    pMenuClick(data) {
+      this.menuInfo.parentId = data.menuId
+      this.menuInfo.parentName = data.name
+      this.pMenuVisible = false
+    },
+    handleClose() {
+      this.$refs.menuForm.resetFields()
+      this.formVisible = false
+    },
+    add() {
+      this.menuInfo = {type:null,parentId:null,parentName:null}
+      this.formVisible = true
+    },
+    edit(id) {
+      this.formVisible = true
+      this.formLoading = true
+      getMenuInfo(id).then(res => {
+        this.menuInfo = res.menu
+        this.formLoading = false
+      })
+    },
+    save() {
+      this.$refs.menuForm.validate((valid) => {
+        if (valid) {
+          this.formLoading = true
+          saveOrUpdateMenu(this.menuInfo).then(res => {
+            this.$message({type:'success',message:'保存成功'})
+            // this.$refs.menuForm.clearValidate()
+            this.$refs.menuForm.resetFields()
+            this.formLoading = false
+            this.formVisible = false
+            this.getMenuList()
+          }).catch(() => {
+            this.formLoading = false
+          })
+        }else {
+          return false;
+        }
+      })
+    },
+    cancel(id) {
+      this.$confirm('确定删除该菜单吗?', '提示', {
+          confirmButtonText: '确定',
+          cancelButtonText: '取消',
+          type: 'warning'
+        }).then(() => {
+          cancelMenu(id).then(res => {
+            this.$message({
+              type: 'success',
+              message: '删除成功!'
+            });
+            this.getMenuList()
+          })
+          
+        })
     }
   },
   mounted() {
     this.getMenuList()
+    this.getSysDictMap(["menu_type"]).then(res => {
+      this.menuType = res.menu_type;
+    })
   }
 };
 </script>
 
+<style rel="stylesheet/scss" lang="scss" scoped>
+</style>
+<style rel="stylesheet/scss" lang="scss">
+.menu-container {
+  padding: 10px;
+  .action-col {
+    display: none;
+  }
+  .el-table tr:hover {
+    .action-col {
+      display: block;
+    }
+  }
+}
+</style>
